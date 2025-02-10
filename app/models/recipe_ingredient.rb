a1ed5a3223e3af27f012217c4adb5044fb954ffe
Eugenio Garcia <eugenio5039@gmail.com>
2025-02-10 17:25:40 -0600

    Remove - recipe from steps and ingredients

--- app/models/recipe_ingredient.rb
+++ app/models/recipe_ingredient.rb
@@ -1,12 +1,10 @@
 # frozen_string_literal: true
 
 class RecipeIngredient < ApplicationRecord
-  belongs_to :recipe
   belongs_to :ingredient
   belongs_to :section
   belongs_to :uom
 
-  validates :recipe_id, presence: true
   validates :ingredient_id, presence: true
   validates :quantity, presence: true
   validates :uom_id, presence: true
