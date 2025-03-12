4d58eb3ec0d90728d22f7c01dfad3841f6180049
Eugenio Garcia <eugenio5039@gmail.com>
2025-03-12 11:46:52 -0600

    Remove - old SCOPES

--- app/controllers/concerns/secured.rb
+++ app/controllers/concerns/secured.rb
@@ -3,53 +3,7 @@
 # Module that manages the scopes and jwt verification
 module Secured
   extend ActiveSupport::Concern
-  # TODO: DELETE SCOPES FORM HERE
-  SCOPES = {
-    '/api/v1/categories/index' => nil,
-    '/api/private' => nil,
-    '/api/private-scoped' => ['read'],
-    'recipes/index' => ['read:recipe'],
-    'recipes/create' => ['create:recipe'],
-    'recipes/show/:id' => ['read:recipe'],
-    'recipes/destroy/:id' => ['delete:recipe'],
-    'sections/create' => ['create:recipe'],
-    'sections/save_multiple' => ['create:recipe', 'update:recipe'],
-    'sections/edit/:id' => ['update:recipe'],
-    'sections/find_by_recipe_id/:id' => ['read:recipe'],
-    'sections/update/:id' => ['update:recipe'],
-    'sections/destroy/:id' => ['delete:recipe'],
-    'steps/create' => ['create:recipe'],
-    'steps/edit/:id' => ['create:recipe', 'update:recipe'],
-    'steps/find_by_recipe_id/:id' => ['read:recipe'],
-    'steps/find_by_section_id/:id' => ['read:recipe'],
-    'steps/update/:id' => ['update:recipe'],
-    'steps/destroy/:id' => ['delete:recipe'],
-    'tags/index' => ['read:tag'],
-    'tags/create' => ['create:tag'],
-    'tags/edit/:id' => ['update:tag'],
-    'tags/update/:id' => ['update:tag'],
-    'tags/destroy/:id' => ['delete:tag'],
-    'categories/index' => ['read:category'],
-    'categories/create' => ['create:category'],
-    'categories/edit/:id' => ['update:category'],
-    'categories/update/:id' => ['update:category'],
-    'categories/destroy/:id' => ['delete:category'],
-    'subcategories/index' => ['read:subcategory'],
-    'subcategories/create' => ['create:subcategory'],
-    'subcategories/edit/:id' => ['update:subcategory'],
-    'subcategories/update/:id' => ['update:subcategory'],
-    'subcategories/destroy/:id' => ['delete:subcategory'],
-    'ingredients/index' => ['read:ingredient'],
-    'ingredients/create' => ['create:ingredient'],
-    'ingredients/edit/:id' => ['update:ingredient'],
-    'ingredients/update/:id' => ['update:ingredient'],
-    'ingredients/destroy/:id' => ['delete:ingredient'],
-    'uoms/index' => ['read:uom'],
-    'uoms/create' => ['create:uom'],
-    'uoms/edit/:id' => ['update:uom'],
-    'uoms/update/:id' => ['update:uom'],
-    'uoms/destroy/:id' => ['delete:uom']
-  }.freeze
+
 
   REQUIRES_AUTHENTICATION = { message: 'Requires authentication' }.freeze
   BAD_CREDENTIALS = {
