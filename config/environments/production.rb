88b783a416f471331017a72925e12691581cffb3
Eugenio Garcia <eugenio5039@gmail.com>
2025-12-30 22:03:13 -0600

    Add host env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,6 +8,12 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
+  # Public API domain (replace later)
+  config.hosts << "recipes-api.eggcloud.com"
+
+  # Internal Docker traffic
+  config.hosts << "backend-rails"
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
